6af20e5583d5a751eb304e19790583099ea8a2d0
Hao Jie <a0125552@u.nus.edu>
2017-02-24 15:44:24 +0800

    Add files via upload

---
+++ apps/grp23/htdocs/main.php
@@ -0,0 +1,130 @@
+<html>
+<head> <title>Main Page</title> </head>
+
+<body>
+<table>
+<tr> <td colspan="2" style="background-color:#FFA500; text-align:center;">
+<h1> Crowd Funding</h1>
+
+
+<!--connect db by using php
+    reference demo-3.php
+-->
+
+<form>
+        ID: <input type="text" name="userID" id="userID">
+        Password: <input type="text" name="password" id="password">
+
+
+        <input type="submit" name="formSubmit" value="Log In" >
+        <input type="hidden" name="inputIsFilled" value="true"/>
+</form>
+</td> </tr>
+
+<tr>
+<td style="background-color:#eeeeee;">
+<form>
+        Project Name: <input type="text" name="projectName" id="projectName">
+        <select name="Category"> <option value="">Select Category</option>
+        <!--print categories by using php
+        	reference demo-3.php
+        < ?php
+        $query = 'SELECT DISTINCT category FROM project';
+        $result = pg_query($query) or die('Query failed: ' . pg_last_error());
+         
+        while($line = pg_fetch_array($result, null, PGSQL_ASSOC)){
+           foreach ($line as $col_value) {
+              echo "<option value=\"".$col_value."\">".$col_value."</option><br>";
+            }
+        }
+        pg_free_result($result);
+        ?>
+        -->
+        </select>
+
+
+        <input type="submit" name="formSubmit" value="Search" >
+        <input type="hidden" name="inputIsFilled" value="true"/>
+</form>
+</td> </tr>
+
+<tr>
+<td style="background-color:#ffffff;">
+<form>
+		<br>
+		Order by: 
+		<?php
+		if($_GET['formIsFilled'] != "true") { 
+		?>
+        	<select name="order"> <option value="Popularity">Popularity</option>
+        		<option value="Money">Money</option>
+        		<option value="Like">Like</option>
+        	</select>
+        <?php
+    	} else { //It was already sorted
+        ?>
+			<select name="order">
+			<?php
+				echo "<option value=\"".$_GET['order']."\">".$_GET['order']."</option>"
+			?>
+				<option value="Popularity">Popularity</option>
+        		<option value="Money">Money</option>
+        		<option value="Like">Like</option>
+        	</select>
+		<?php
+		}
+		?>
+
+        <input type="submit" name="sorting" value="Select"/>
+        <input type="hidden" name="formIsFilled" value="true"/>
+		<br><br>
+        
+        <!--print projects by using php
+        ADD (title, category condition)
+
+        $query = 'SELECT p.pid FROM project ORDER BY ?';
+        $result = pg_query($query) or die('Query failed: ' . pg_last_error());
+
+        if(".$_GET['order']." = "Money") {
+        	$query = 'SELECT category FROM project ORDER BY ??';
+        	$result = pg_query($query) or die('Query failed: ' . pg_last_error());
+        }
+        else if(".$_GET['order']." = "Like") {
+        	$query = 'SELECT category FROM project ORDER BY ???';
+        	$result = pg_query($query) or die('Query failed: ' . pg_last_error());
+        }
+         
+        while($line = pg_fetch_array($result, null, PGSQL_ASSOC)){
+           foreach ($line as $col_value) {
+              echo "<option value=\"".$col_value."\">".$col_value."</option><br>";
+            }
+        }
+        pg_free_result($result);
+        -->
+
+        <!--It is just for print-->
+        <input type="submit" name="projectID" id="Format1" value="project1" style="height:50px; width:100px"/>
+        <input type="submit" name="projectID" id="Format2" value="project2" style="height:50px; width:100px"/>
+        <input type="submit" name="projectID" id="Format3" value="project3" style="height:50px; width:100px"/>
+        <input type="submit" name="projectID" id="Format4" value="project4" style="height:50px; width:100px"/>
+        <input type="submit" name="projectID" id="Format5" value="project5" style="height:50px; width:100px"/>
+        <input type="submit" name="projectID" id="Format6" value="project6" style="height:50px; width:100px"/>
+
+        <?php
+        if($_GET['projectID'] == "project1") //redirection
+        {
+        	header('Location: /helloworld.php'); exit();
+        }
+        ?>
+
+        
+</form>
+</td> </tr>
+
+<tr>
+<td colspan="2" style="background-color:#FFA500; text-align:center;"> Copyright &#169; CS2102_PROJECT23
+</td> </tr>
+</table>
+
+</body>
+</html>
---
+++ apps/grp23/htdocs/register.php
@@ -0,0 +1,137 @@
+<?php
+    ob_start();
+    session_start();
+    if( isset($_SESSION['userName'])!="" ){
+        header("Location: home.php");
+    }
+
+    include_once 'dbconnect.php';
+
+
+    $error = false;
+
+    if (isset($_POST['submit']) ) {
+
+        // clean user inputs to prevent sql injections
+        $name = sanitize($_POST['userFullName']);
+        $userName = sanitize($_POST['userName']);
+        $email = sanitize($_POST['email']);
+        $newPassword = sanitize($_POST['newPassword']);
+        $password = sanitize($_POST['password']);
+
+        // basic name validation
+        if (empty($name)) {
+            $error = true;
+            $nameError = "Please enter your full name.";
+        } else if (strlen($name) < 3) {
+            $error = true;
+            $nameError = "Name must have at least 3 characters.";
+        } else if (!preg_match("/^[a-zA-Z ]+$/",$name)) {
+            $error = true;
+            $nameError = "Name must contain alphabets and space.";
+        }
+
+        // basic username validation
+        if (empty($userName)) {
+            $error = true;
+            $userNameError = "Please enter a valid user name .";
+        } else if (strlen($userName) < 6) {
+            $error = true;
+            $userNameError = "Name must have at least 6 characters.";
+        } else if (!preg_match("/[a-zA-Z]/", $userName)) {
+            $error = true;
+            $userNameError = "Name must contain alphabets.";
+        }
+        else {
+            // check username exist or not
+            $query = "SELECT userID FROM users WHERE userEmail='$userName'";
+            $result = pg_query($query);
+            $count = pg_num_rows($result);
+            if($count!=0){
+                $error = true;
+                $emailError = "Username is already in use.";
+            }
+        }
+
+        //basic email validation
+        if ( !filter_var($email,FILTER_VALIDATE_EMAIL) ) {
+            $error = true;
+            $emailError = "Please enter your email address.";
+        } else {
+            // check email exist or not
+            $query = "SELECT userEmail FROM users WHERE userEmail='$email'";
+            $result = pg_query($query);
+            $count = pg_num_rows($result);
+            if($count!=0){
+                $error = true;
+                $emailError = "Email is already in use.";
+            }
+        }
+        
+        // password validation
+        if (empty($password) || empty($newPassword)){
+            $error = true;
+            $passwordError = "Please enter password.";
+        } else if(strlen($password) < 6) {
+            $error = true;
+            $passwordError = "Password must have at least 6 characters.";
+        }
+        else if($password != $newPassword) {
+            $error = true;
+            $passwordError = "You have entered the wrong password.";
+        }
+
+        // password encrypt using SHA256();
+        $password = hash('sha256', $password);
+
+        // if there's no error, continue to signup
+        if( !$error ) {
+            $query = "INSERT INTO users VALUES('$userName', '$password', 'FALSE', '$email','$name')";
+
+            $res = pg_query($query) or die('Query failed: ' . pg_last_error());
+
+            if ($res) {
+                unset($name);
+                unset($email); 
+                unset($userName);
+                unset($password);
+                header("Location: thank-you-registration.php");
+            } else {
+                $errMSG = "Something went wrong, try again."; 
+            } 
+
+        }
+
+    }
+
+    function sanitize($data) {
+        $data = trim($data);
+        $data = strip_tags($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
+
+?>
+
+
+<!DOCTYPE HTML>  
+<html>
+    <body>  
+        <h1>Sign Up </h1>
+        <form method="post" action="<?php echo $_POST['PHP_SELF']; ?>">
+            Name: <input type="text" name="userFullName" value="<?php echo $name;?>"><span style="color:red";><?php echo $nameError;?></span>	
+            <br>
+            Username: <input type="text" name="userName" value="<?php echo $userName;?>"><span style="color:red";><?php echo $userNameError;?></span>
+            <br>
+            Email: <input type="text" name="email" value="<?php echo $email;?>"><span style="color:red";><?php echo $emailError;?></span>
+            <br>
+            New Password: <input type="password" name="newPassword" value="<?php echo $newPassword;?>"><span style="color:red";><?php echo $passwordError;?></span>
+            <br>
+            Confirmed Password: <input type="password" name="password" value="<?php echo $password;?>"><span style="color:red";><?php echo $passwordError;?></span>
+            <br><br>
+            <input type="submit" name="submit" value="Submit"><span style="color:red";><?php echo $errMSG; ?></span><br> 
+
+        </form>
+    </body>
+</html>
+
