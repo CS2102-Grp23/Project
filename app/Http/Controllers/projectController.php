8993abb8f21c88b280ef1ec28a9b547c3706d815
Hao Jie <a0125552@u.nus.edu>
2017-04-05 22:12:01 +0800

    Remove redundant condition at line 188

--- app/Http/Controllers/projectController.php
+++ app/Http/Controllers/projectController.php
@@ -185,7 +185,7 @@ class projectController extends BaseController {
     $contribution = sanitize($contribution);
     //$projectID = $req->input('projectID');
 
-    if (empty($contribution) || $contribution <= 0) {
+    if ($contribution <= 0) {
       $error = true;
       $errorMessage = "Please enter a valid contribution.";
       return $errorMessage;
